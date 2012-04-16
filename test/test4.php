01573c0062ed431b7d1cadcf57b33c54281dd7b4
Gabriel <widmogrod@gmail.com>
2012-04-16 21:20:30 +0200

    retrive keywords using & prioritizing by tags name

--- test/test4.php
+++ test/test4.php
@@ -171,6 +171,11 @@ namespace Tagger
 
         protected $_disableLibXmlErrors = true;
 
+        /**
+         * @var HtmlPriority
+         */
+        protected $_priority;
+
         public function retrieveTags($html)
         {
             $document = $this->getDocument();
@@ -189,42 +194,66 @@ namespace Tagger
                 throw new Exception\Exception($message);
             }
 
-            $titleWords = array();
-            $metaWords = array();
-            $headerWords = array();
-            $strongWords = array();
-            $anchorWords = array();
-
             $text = new PlainText();
 
-            $content = $this->getTextContentForPath('//html//title');
-            $titleWords = $text->retrieveTags($content);
+            $priority = $this->getPriority();
+
+            $elements = $document->getElementsByTagName('*');
+            foreach ($elements as /* @var $element \DOMElement */ $element)
+            {
+                $tagName = trim($element->tagName);
+                switch ($tagName)
+                {
+//                    case 'ul':
+                    case 'br':
+                    case 'hr':
+                    case 'table':
+                    case 'html':
+                    case 'head':
+                    case 'meta':
+                    case 'body':
+                    case 'link':
+                    case 'style':
+                    case 'script':
+                        break;
+
+                    default:
+
+                        $content = null;
+                        switch($tagName)
+                        {
+                            case 'img': $content = $element->getAttribute('alt'); break;
+                        }
+
+                        if ($content && !$element->childNodes->length) {
+                            continue 2;
+                        }
+
+                        if (!$content) {
+                            $content = $element->textContent;
+                        }
+
+                        $words = $text->retrieveTags($content);
+                        $priority->addWordsForTag($this->filter($words), $tagName);
+                }
+            }
 
             // TODO install lower-case function & use it!
             // $content = $this->getTextContentForPath("//meta[contains(lower-case(@name), 'description')]", 'content');
             $content = $this->getTextContentForPath("//meta[contains(@name, 'description')]", 'content');
-            $metaWords = $text->retrieveTags($content);
+            $words = $text->retrieveTags($content);
+            $priority->addWordsForTag($words, 'meta[name="description"]');
 
             $content = $this->getTextContentForPath("//meta[contains(@name, 'keywords')]", 'content');
-            $metaWords = array_merge($metaWords, $text->retrieveTags($content));
+            $words = $text->retrieveTags($content);
+            $priority->addWordsForTag($words, 'meta[name="keywords"]');
+        }
 
-            $content = $this->getTextContentForPath('//h1|//h2|//h3|//h4|//h5|//h6');
-            $headerWords = $text->retrieveTags($content);
-
-            $content = $this->getTextContentForPath('//strong|//b');
-            $strongWords = $text->retrieveTags($content);
-
-            $content = $this->getTextContentForPath('//a');
-            $anchorWords = $text->retrieveTags($content);
-
-
-            $result = array_merge($metaWords, $headerWords, $strongWords, $anchorWords);
-
-            $result = array_filter($result, function($value){
+        private function filter(array $result)
+        {
+            return array_filter($result, function($value){
                 return mb_strlen($value) >= 3 && !is_numeric($value);
             });
-
-            return $result;
         }
 
         private function getTextContentForPath($path, $attributeName = null)
@@ -269,9 +298,30 @@ namespace Tagger
         {
             return $this->_disableLibXmlErrors;
         }
+
+        public function getPriority()
+        {
+//            if (null === $this->_priority) {
+//                $this->_priority = new HtmlPriority();
+//            }
+            return $this->_priority;
+        }
+
+        /**
+         * @param HtmlPriority $priority
+         */
+        public function setPriority(HtmlPriority $priority)
+        {
+            $this->_priority = $priority;
+        }
     }
 
-    class Priority
+    interface Priority
+    {
+        public function getPriority($word);
+    }
+
+    class SimplePriority implements Priority
     {
         protected $_numberPriority;
         protected $_numberLangthPriority = array();
@@ -356,6 +406,126 @@ namespace Tagger
         }
     }
 
+    class HtmlPriority implements Priority
+    {
+        const WORD_TOTAL_PRIORITY = '__all__';
+        const DEFAULT_PRIORITY = 1;
+
+        protected $_wordPriority = array();
+
+        protected $_tagNamePriority = array();
+
+        protected $_blackListWords = array();
+
+        protected $_numberPriority;
+        protected $_numberLangthPriority = array();
+
+        protected $_stringPriority;
+        protected $_stringLangthPriority = array();
+
+        public function getPriority($word)
+        {
+            $word = mb_strtolower($word);
+            if (isset($this->_blackListWords[$word])) {
+                return 0;
+            }
+
+            if (isset($this->_wordPriority[$word])) {
+                return $this->_wordPriority[$word][self::WORD_TOTAL_PRIORITY];
+            }
+
+            $length = mb_strlen($word);
+
+            return is_numeric($word)
+                ? $this->getPriorityForNumber($length)
+                : $this->getPriorityForString($length);
+        }
+
+        public function setTagNamePriority($tagName, $priority)
+        {
+            $tagName = strtolower($tagName);
+            $this->_tagNamePriority[$tagName] = (int) $priority;
+        }
+
+        public function getTagNamePriority($tagName)
+        {
+            $tagName = strtolower($tagName);
+            return array_key_exists($tagName, $this->_tagNamePriority)
+                ? $this->_tagNamePriority[$tagName]
+                : self::DEFAULT_PRIORITY;
+        }
+
+        public function addWordsForTag(array $words, $tagName)
+        {
+            while ($word = array_pop($words)) {
+                $this->addWordForTag($word, $tagName);
+            }
+        }
+
+        public function addWordForTag($word, $tagName)
+        {
+            $word = mb_strtolower($word);
+            if (!array_key_exists($word, $this->_wordPriority)) {
+                $this->_wordPriority[$word] = array(
+                    self::WORD_TOTAL_PRIORITY => self::DEFAULT_PRIORITY
+                );
+            }
+
+            $priority = $this->getTagNamePriority($tagName);
+            $this->_wordPriority[$word][$tagName] = $priority;
+            $this->_wordPriority[$word][self::WORD_TOTAL_PRIORITY] += $priority;
+        }
+
+        public function addWordsToBlackList(array $words)
+        {
+            array_map(array($this, 'addWordToBlackList'), $words);
+        }
+
+        public function addWordToBlackList($word)
+        {
+            $this->_blackListWords[$word] = true;
+        }
+
+        public function setPriorityForNumber($priority)
+        {
+            $this->_numberPriority = (int) $priority;
+        }
+
+        public function setPriorityForNumerLength($length, $priority)
+        {
+            $this->_numberLangthPriority[abs((int) $length)] = (int) $priority;
+        }
+
+        public function setPriorityForString($priority)
+        {
+            $this->_stringPriority = $priority;
+        }
+
+        public function setPriorityForStingLength($length, $priority)
+        {
+            $this->_stringLangthPriority[abs((int) $length)] = (int) $priority;
+        }
+
+        public function getPriorityForNumber($length = null)
+        {
+            return ($length > 0 && array_key_exists($length, $this->_numberLangthPriority))
+                ? $this->_numberLangthPriority[$length]
+                : $this->_numberPriority;
+        }
+
+        public function getPriorityForString($length = null)
+        {
+            return ($length > 0 && array_key_exists($length, $this->_stringLangthPriority))
+                ? $this->_stringLangthPriority[$length]
+                : $this->_stringPriority;
+        }
+
+//        public function __destruct()
+//        {
+//            var_dump($this->_wordPriority);
+//        }
+    }
+
     /**
      * Test
      */
@@ -365,8 +535,12 @@ namespace Tagger
     $blackListWords = array_filter($blackListWords);
 
     $html = file_get_contents(__DIR__ . '/_data/laksa.html');
-    $html = file_get_contents(__DIR__ . '/_data/blog.widmogrod.html');
+//    $html = file_get_contents(__DIR__ . '/_data/blog.widmogrod.html');
 //    $html = file_get_contents(__DIR__ . '/_data/andredom.pl.html');
+//    $html = file_get_contents(__DIR__ . '/_data/matejko.html');
+//    $html = file_get_contents(__DIR__ . '/_data/mostowy.com.pl.html');
+
+
 
     $encoding = mb_detect_encoding($html);
     $oldEncoding = mb_internal_encoding();
@@ -374,53 +548,85 @@ namespace Tagger
 
     $html = html_entity_decode($html, null, $encoding);
 
-    $priority = new Priority();
+//    $priority = new SimplePriority();
+//    $priority->setPriorityForNumber(5);
+//    $priority->setPriorityForString(20);
+//    $priority->setPriorityForStingLength(1, 0);
+//    $priority->setPriorityForStingLength(2, 2);
+//    $priority->setPriorityForStingLength(3, 4);
+//    $priority->setPriorityForStingLength(4, 8);
+//    $priority->setPriorityForStingLength(5, 16);
+//
+//    $priority->addWordsToBlackList($blackListWords);
+
+
+    $priority = new HtmlPriority();
+    $priority->addWordsToBlackList($blackListWords);
     $priority->setPriorityForNumber(5);
     $priority->setPriorityForString(20);
     $priority->setPriorityForStingLength(1, 0);
     $priority->setPriorityForStingLength(2, 2);
-    $priority->setPriorityForStingLength(3, 4);
-    $priority->setPriorityForStingLength(4, 8);
-    $priority->setPriorityForStingLength(5, 16);
 
-    $priority->addWordsToBlackList($blackListWords);
-
+    $priority->setTagNamePriority('a', 25);
+    $priority->setTagNamePriority('b', 27);
+    $priority->setTagNamePriority('strong', 27);
+    $priority->setTagNamePriority('h6', 27);
+    $priority->setTagNamePriority('h5', 29);
+    $priority->setTagNamePriority('h4', 31);
+    $priority->setTagNamePriority('h3', 35);
+    $priority->setTagNamePriority('h2', 41);
+    $priority->setTagNamePriority('h1', 70);
+    $priority->setTagNamePriority('title', 120);
+    $priority->setTagNamePriority('meta[name="description"]', 120);
+    $priority->setTagNamePriority('meta[name="keywords"]', 120);
 
     $t = new Html();
+    $t->setPriority($priority);
     $words = $t->retrieveTags($html);
-    $priority->addWordsToPriorityList($words, 40);
+
+//    $priority->addWordsToPriorityList($words, 40);
 //var_dump($words);
 
-    $words = array_count_values($words);
-    $words = array_keys($words);
+//    $words = array_count_values($words);
+//    $words = array_keys($words);
 //var_dump($words);
 
-    $s = new Similarity($words);
 //    $similar = $s->toArray();
 //var_dump($s->toArray(false));
 //    $priority->addWordsToPriorityList($s->toArray(true), 80);
 
 
     $t = new PlainText();
-    $summary = $t->retrieveTags($html);
+    $words = $t->retrieveTags($html);
+
+    $summary = $words;
     $summary = array_count_values($summary);
     asort($summary, SORT_STRING);
+
+    $s = new Similarity($words);
+//    $summary = $s->toArray();
 
     $priorityQueue = new \SplPriorityQueue();
     foreach ($summary as $word => $occurrences)
     {
         $priorityNumber = 1;
 
-        $similarWords = $s->getSimilarWordsTo($word);
-//        $priorityNumber += count($similarWords);
         $priorityNumber *= $priority->getPriority($word);
-//        $priorityNumber = pow($priorityNumber, $priority->getPriority($word));
-        $priorityNumber += array_sum(array_map(array($priority,'getPriority'), $similarWords));
-//        $priorityNumber += count($similarWords);
-//        $priorityNumber = log($priorityNumber);
+        if ($priorityNumber == 0) {
+            goto insert;
+        }
 
+        $similarWords = $s->getSimilarWordsTo($word);
+        $priorityNumber += array_reduce(array_map(array($priority,'getPriority'), $similarWords), function($a, $b) {
+            return $a * $b;
+        });
+
+        insert:
         $priorityQueue->insert($word, $priorityNumber);
     }
+
+//    $priorityQueue = new \IteratorIterator($priorityQueue);
+//    $priorityQueue = new \LimitIterator($priorityQueue, 20);
 
     foreach ($priorityQueue as $word) {
         var_dump($word);
